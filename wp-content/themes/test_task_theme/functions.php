69334ddb725a12f029779928314c28de576d2785
Andrey Ignatov aka Cessel <cessel@yandex.ru>
2023-03-05 12:42:11 +0300

    Add jquery library

--- wp-content/themes/test_task_theme/functions.php
+++ wp-content/themes/test_task_theme/functions.php
@@ -1,12 +1,13 @@
 <?php
-/* АВТОМАТИЧЕСКОЕ ПОДКЛЮЧЕНИЕ JS И CSS ФАЙЛОВ ИЗ ПАПКИ /js/ и /css/ СООТВЕТСТВЕННО */
-/*Для включения кеша закомментировать $v = time(); */
+/* ПОДКЛЮЧЕНИЕ JS И CSS ФАЙЛОВ ИЗ ПАПКИ /js/ и /css/ СООТВЕТСТВЕННО */
+/* Для включения кеша закомментировать $v = time(); */
 
 function CWG_scripts() {
 	$v       = time();
 	$uri_css = get_template_directory_uri() . '/assets/css';
 	$uri_js  = get_template_directory_uri() . '/assets/js';
 	wp_deregister_script( 'jquery' );
+	wp_register_script('jquery',$uri_js.'misc.js','',$v);
 }
 
 add_action( 'wp_enqueue_scripts', 'CWG_scripts' );
