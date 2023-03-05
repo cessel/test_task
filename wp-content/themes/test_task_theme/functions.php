1fc3746ee07d782d2f25c3ec4c5bcd629633eed0
Andrey Ignatov aka Cessel <cessel@yandex.ru>
2023-03-05 12:48:25 +0300

    Update misc.js

--- wp-content/themes/test_task_theme/functions.php
+++ wp-content/themes/test_task_theme/functions.php
@@ -7,7 +7,7 @@ function CWG_scripts() {
 	$uri_css = get_template_directory_uri() . '/assets/css';
 	$uri_js  = get_template_directory_uri() . '/assets/js';
 	wp_deregister_script( 'jquery' );
-	wp_enqueue_script('jquery',$uri_js.'misc.js','',$v);
+	wp_enqueue_script('jquery',$uri_js.'/misc.js','',$v);
 }
 
 add_action( 'wp_enqueue_scripts', 'CWG_scripts' );
