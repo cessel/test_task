b45be0747ca160d888da42c5eec5b26332102b83
Andrey Ignatov aka Cessel <cessel@yandex.ru>
2023-03-05 13:47:22 +0300

    Update node js

--- wp-content/themes/test_task_theme/assets/js/misc.js
+++ wp-content/themes/test_task_theme/assets/js/misc.js
@@ -1,4 +1,6 @@
-var $ = require( "jquery" );
+const { JSDOM } = require( "../node_modules/jsdom/lib/api.js" );
+const { window } = new JSDOM( "" );
+const $ = require( "../node_modules/jquery/dist/jquery.min.js" )( window );
 
 console.log($);
 console.log(jQuery);
